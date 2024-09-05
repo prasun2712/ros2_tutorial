3f33873c37dac97061ec530806525b0d56f641e7
Prasun <prasun.ieee@gmail.com>
2024-09-05 22:43:11 +0530

    Adding comments in subscriber.

--- ros2_ws/src/cpp_pubsub/src/subscriber_member_function.cpp
+++ ros2_ws/src/cpp_pubsub/src/subscriber_member_function.cpp
@@ -6,26 +6,29 @@ using std::placeholders::_1;
 
 class MinimalSubscriber : public rclcpp::Node
 {
-  public:
+public:
+    // There is no timer because the subscriber simply responds whenever data is published to the topic topic.
     MinimalSubscriber()
-    : Node("minimal_subscriber")
+        : Node("minimal_subscriber")
     {
-      subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+        subscription_ = this->create_subscription<std_msgs::msg::String>(
+            "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
     }
 
-  private:
+private:
+    // The topic_callback function receives the string message data published over the topic.
     void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
     {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
     }
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    // The main function is exactly the same, except now it spins the MinimalSubscriber node.
+    rclcpp::spin(std::make_shared<MinimalSubscriber>());
+    rclcpp::shutdown();
+    return 0;
 }

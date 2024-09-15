bebb3eccb0550029b0a002d9f7a4044031b8ceab
Prasun <prasun.ieee@gmail.com>
2024-09-15 23:44:46 +0530

    Added simple client example for adding two integers in ROS2.

--- ros2_ws/src/cpp_srvcli/src/add_two_ints_server.cpp
+++ ros2_ws/src/cpp_srvcli/src/add_two_ints_server.cpp
@@ -4,25 +4,26 @@
 #include <memory>
 
 void add(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
-          std::shared_ptr<example_interfaces::srv::AddTwoInts::Response>      response)
+         std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response)
 {
-  response->sum = request->a + request->b;
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld",
+    response->sum = request->a + request->b;
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld"
+                                              " b: %ld",
                 request->a, request->b);
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
 }
 
 int main(int argc, char **argv)
 {
-  rclcpp::init(argc, argv);
+    rclcpp::init(argc, argv);
 
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_server");
+    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_server");
 
-  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service =
-    node->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", &add);
+    rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service =
+        node->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", &add);
 
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add two ints.");
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add two ints.");
 
-  rclcpp::spin(node);
-  rclcpp::shutdown();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
 }

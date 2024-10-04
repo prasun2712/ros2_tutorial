476e23e390e4da11462a86dbfb72776ebf292e3a
Prasun <prasun.ieee@gmail.com>
2024-10-04 23:50:48 +0530

    Added comments and description for server node.

--- ros2_ws/src/cpp_srvcli/src/add_two_ints_server.cpp
+++ ros2_ws/src/cpp_srvcli/src/add_two_ints_server.cpp
@@ -1,8 +1,10 @@
+// The first two #include statements are your package dependencies.
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/srv/add_two_ints.hpp"
 
 #include <memory>
 
+// The add function adds two integers from the request and gives the sum to the response, while notifying the console of its status using logs.
 void add(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
          std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response)
 {
@@ -15,15 +17,20 @@ void add(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> req
 
 int main(int argc, char **argv)
 {
+    // Initializes ROS 2 C++ client library:
     rclcpp::init(argc, argv);
 
+    // Creates a node named add_two_ints_server:
     std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_server");
-
+    
+    // Creates a service named add_two_ints for that node and automatically advertises it over the networks with the &add method:
     rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service =
         node->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", &add);
-
+    
+    // Prints a log message when it’s ready:
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add two ints.");
 
+    // Spins the node, making the service available.
     rclcpp::spin(node);
     rclcpp::shutdown();
 }

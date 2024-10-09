304afacae6ad747c7aee7fdd03e2efa660866871
Prasun <prasun.ieee@gmail.com>
2024-10-09 18:31:20 +0530

    Added example for python client node.

--- ros2_ws/src/py_srvcli/setup.py
+++ ros2_ws/src/py_srvcli/setup.py
@@ -21,6 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'service = py_srvcli.service_member_function:main',
+            'client = py_srvcli.client_member_function:main',
         ],
     },
 )

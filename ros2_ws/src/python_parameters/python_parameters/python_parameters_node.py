4583c01c89e4a16b0439a6bd6c358dd5c5d087e7
Prasun <prasun.ieee@gmail.com>
2024-10-10 23:10:48 +0530

    Added comments for python parameter node.

--- ros2_ws/src/python_parameters/python_parameters/python_parameters_node.py
+++ ros2_ws/src/python_parameters/python_parameters/python_parameters_node.py
@@ -1,6 +1,11 @@
+# The import statements at the top are used to import the package dependencies.
 import rclpy
 import rclpy.node
 
+# The next piece of code creates the class and the constructor. 
+# The line self.declare_parameter('my_parameter', 'world') of the constructor creates a parameter with the name my_parameter and a default value of world. 
+# The parameter type is inferred from the default value, so in this case it would be set to a string type. 
+# Next the timer is initialized with a period of 1, which causes the timer_callback function to be executed once a second.
 class MinimalParam(rclpy.node.Node):
     def __init__(self):
         super().__init__('minimal_param_node')
@@ -9,6 +14,10 @@ class MinimalParam(rclpy.node.Node):
 
         self.timer = self.create_timer(1, self.timer_callback)
 
+    # The first line of our timer_callback function gets the parameter my_parameter from the node, and stores it in my_param.
+    # Next the get_logger function ensures the event is logged. 
+    # The set_parameters function then sets the parameter my_parameter back to the default string value world. 
+    # In the case that the user changed the parameter externally, this ensures it is always reset back to the original.
     def timer_callback(self):
         my_param = self.get_parameter('my_parameter').get_parameter_value().string_value
 
@@ -22,6 +31,8 @@ class MinimalParam(rclpy.node.Node):
         all_new_parameters = [my_new_param]
         self.set_parameters(all_new_parameters)
 
+# Following the timer_callback is our main. Here ROS 2 is initialized, an instance of the MinimalParam class is constructed, 
+# and rclpy.spin starts processing data from the node.
 def main():
     rclpy.init()
     node = MinimalParam()

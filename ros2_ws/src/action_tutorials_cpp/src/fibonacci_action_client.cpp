3b656b62f0f0ebf710b33338f17d41da95d1ddb6
Prasun <prasun.ieee@gmail.com>
2024-10-12 14:20:01 +0530

    Added comment for cpp fibonacci action tutorial client.

--- ros2_ws/src/action_tutorials_cpp/src/fibonacci_action_client.cpp
+++ ros2_ws/src/action_tutorials_cpp/src/fibonacci_action_client.cpp
@@ -1,3 +1,4 @@
+// The first few lines include all of the headers we need to compile.
 #include <functional>
 #include <future>
 #include <memory>
@@ -12,24 +13,43 @@
 
 namespace action_tutorials_cpp
 {
+    // Next we create a class that is a derived class of rclcpp::Node
     class FibonacciActionClient : public rclcpp::Node
     {
     public:
         using Fibonacci = action_tutorials_interfaces::action::Fibonacci;
         using GoalHandleFibonacci = rclcpp_action::ClientGoalHandle<Fibonacci>;
 
+        // The constructor for the FibonacciActionClient class initializes the node name as fibonacci_action_client
         explicit FibonacciActionClient(const rclcpp::NodeOptions &options)
             : Node("fibonacci_action_client", options)
         {
+            /*
+            The constructor also instantiates a new action client
+
+            An action client requires 3 things:
+             * The templated action type name: Fibonacci.
+             * A ROS 2 node to add the action client to: this.
+             * The action name: 'fibonacci'.
+            */
             this->client_ptr_ = rclcpp_action::create_client<Fibonacci>(
                 this,
                 "fibonacci");
 
+            // Instantiate a ROS timer that will kick off the one and only call to send_goal. When the timer expires, it will call send_goal.
             this->timer_ = this->create_wall_timer(
                 std::chrono::milliseconds(500),
                 std::bind(&FibonacciActionClient::send_goal, this));
         }
 
+        /*
+        This function does the following:
+         * Cancels the timer (so it is only called once).
+         * Waits for the action server to come up.
+         * Instantiates a new Fibonacci::Goal.
+         * Sets the response, feedback, and result callbacks.
+         * Sends the goal to the server.
+        */
         void send_goal()
         {
             using namespace std::placeholders;
@@ -61,6 +81,7 @@ namespace action_tutorials_cpp
         rclcpp_action::Client<Fibonacci>::SharedPtr client_ptr_;
         rclcpp::TimerBase::SharedPtr timer_;
 
+        // When the server receives and accepts the goal, it will send a response to the client. That response is handled by goal_response_callback.
         void goal_response_callback(const GoalHandleFibonacci::SharedPtr &goal_handle)
         {
             if (!goal_handle)
@@ -73,6 +94,7 @@ namespace action_tutorials_cpp
             }
         }
 
+        // Assuming the goal was accepted by the server, it will start processing. Any feedback to the client will be handled by the feedback_callback.
         void feedback_callback(
             GoalHandleFibonacci::SharedPtr,
             const std::shared_ptr<const Fibonacci::Feedback> feedback)
@@ -86,6 +108,7 @@ namespace action_tutorials_cpp
             RCLCPP_INFO(this->get_logger(), ss.str().c_str());
         }
 
+        // When the server is finished processing, it will return a result to the client. The result is handled by the result_callback.
         void result_callback(const GoalHandleFibonacci::WrappedResult &result)
         {
             switch (result.code)
